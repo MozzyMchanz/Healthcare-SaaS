a2454bf1d72019cf1e305175ba373230027f3ec9
MozzyMchanz <mozzymchanz@gmail.com>
2026-03-27 16:19:25 +0300

    commi

--- backend/routes/appointments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const appointmentController = require('../controllers/appointmentController');
-
-router.get('/', appointmentController.getAppointments);
-router.post('/', appointmentController.createAppointment);
-
-module.exports = router;
-
--- backend/routes/doctors.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const doctorController = require('../controllers/doctorController');
-
-router.get('/', doctorController.getDoctors);
-router.post('/', doctorController.createDoctor);
-
-module.exports = router;
-
